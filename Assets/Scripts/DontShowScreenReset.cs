ac289513f2045cdd0e8d9395a8d033f9ad2fb43f
naichilab <naichilab@live.jp>
2017-11-14 03:05:24 +0900

    背景おいただけ

---
+++ Assets/Scripts/DontShowScreenReset.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+public class DontShowScreenReset : MonoBehaviour
+{
+    [Range(0, 10)]
+    public float
+        speed = 10;
+    public int spriteCount = 3;
+    Vector3 spriteSize;
+    void Start()
+    {
+        spriteSize = GetComponent<SpriteRenderer>().bounds.size;
+    }
+    void Update()
+    {
+        transform.position += Vector3.left * speed * Time.deltaTime;
+#if UNITY_EDITOR
+        var spritex = (transform.position + spriteSize / 2).x;
+        if (spritex < ScreenManager.Instance.screenRect.x)
+        {
+            OnBecameInvisible();
+        }
+#endif
+    }
+    void OnBecameInvisible()
+    {
+        float width = GetComponent<SpriteRenderer>().bounds.size.x;
+        transform.position += Vector3.right * width * spriteCount;
+    }
+}
